4cae93ddd8afb17b1f2f9d6761edd4489717ea30
Richshorrock <131653895+Richshorrock@users.noreply.github.com>
2023-06-14 16:43:45 +0200

    updated supermarket with inventory

--- supermarket.py
+++ supermarket.py
@@ -3,3 +3,9 @@ class Supermarket:
     locations = ['dairy', 'spices', 'fruit', 'drinks', 'checkout']
     
     
+#product_dictionaries
+    dairy_products = {"dairy_retail_price": [2, 3, 5]}
+    spice_products = {"spice_retail_price": [1, 2, 3]}
+    fruit_products = {"fruit_retail_price": [1, 3, 6]}
+    drink_products = {"drink_retail_price": [2, 5, 10]}
+    
