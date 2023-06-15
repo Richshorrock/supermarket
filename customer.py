3d1a892ceb5afbe5954242c00b3ec892a6fc4a2b
Richshorrock <131653895+Richshorrock@users.noreply.github.com>
2023-06-15 14:03:58 +0200

    updating

--- customer.py
+++ customer.py
@@ -1,15 +1,41 @@
 import pandas as pd
+import numpy as np
 
-locations = ['dairy', 'spices', 'fruit', 'drinks', 'checkout']
+asiles = ['dairy', 'spices', 'fruit', 'drinks', 'checkout']
 
+stock = {
+    "dairy_retail_price": [2, 3, 5],
+    "spice_retail_price": [1, 2, 3],
+    "fruit_retail_price": [1, 3, 6],
+    "drink_retail_price": [2, 5, 10]
+}
+    
 class Customer:
     
-    def __init__(self, customer_id, basket):
+    def __init__(self, customer_id, location, basket):
         self.customer_id = customer_id
-        self.supermarket = supermarket
         self.location = None
         self.basket = []
 
-    def move(self):
-        self.location += 0.1
+    def move(self, location):
+        self.location = np.random.choice(self.asiles).self??????
+        
+    def fill_basket(self, item)
+        self.location == 'dairy'
+        item = np.choice(stock["dairy_ retail_price"])
+        self.basket.append(item)
+        
+        self.location == 'spice'
+        item = np.choice(stock["spice retail_price"])
+        self.basket.append(item)
+        
+        self.location == 'fruit'
+        item = np.choice(stock["fruit retail_price"])
+        self.basket.append(item)
+        
+        self.location == 'drinks'
+        item = np.choice(stock["drink_retail_price"])
+        self.basket.append(item)
+        
+        
         
