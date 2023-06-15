a42273532e93fe92327aa3ef2eea1cd18daeadff
Richshorrock <131653895+Richshorrock@users.noreply.github.com>
2023-06-15 11:44:48 +0200

    updaitng

--- customer.py
+++ customer.py
@@ -1,25 +1,15 @@
 import pandas as pd
 
-### importing customer no's from data
-
-df=pd.read_csv=('data/monday.csv')
-
-def get_customer_id(customer_id):
-    customer_row = df[df['customer_no'] == customer_id]
-  
+locations = ['dairy', 'spices', 'fruit', 'drinks', 'checkout']
 
 class Customer:
     
     def __init__(self, customer_id, basket):
         self.customer_id = customer_id
         self.supermarket = supermarket
-        self.name = get_customer_id(customer_id)
         self.location = None
         self.basket = []
 
-    #def move(self):
-        #self.location += 0.1
+    def move(self):
+        self.location += 0.1
         
-customer1 = Customer()
-
-print(customer1.customer_id)
